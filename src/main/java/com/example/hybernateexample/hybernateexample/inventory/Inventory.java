3fc140b574eaf9262431d89d39c8fd6f668428eb
UmenR <umendrarajapakshe@gmail.com>
2023-01-12 15:12:59 +1100

    feat: initial implementation of inventory

---
+++ src/main/java/com/example/hybernateexample/hybernateexample/inventory/Inventory.java
@@ -0,0 +1,42 @@
+package com.example.hybernateexample.hybernateexample.inventory;
+
+import com.example.hybernateexample.hybernateexample.film.Film;
+
+import javax.persistence.*;
+
+@Entity
+public class Inventory {
+    @Id
+    @Column(name = "inventory_id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    private int store_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public int getStore_id() {
+        return store_id;
+    }
+
+    public void setStore_id(int store_id) {
+        this.store_id = store_id;
+    }
+}
---
+++ src/main/java/com/example/hybernateexample/hybernateexample/inventory/InventoryController.java
@@ -0,0 +1,4 @@
+package com.example.hybernateexample.hybernateexample.inventory;
+
+public class InventoryController {
+}
